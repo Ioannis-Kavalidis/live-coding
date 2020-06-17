f5c9c421ad23d6e5ad7506290957566440ea34b3
Hadi Nsreeny <hnsreeny@gmail.com>
2020-06-17 14:32:26 +0200

    instagram app

--- jun/instagram-clone/src/App.js
+++ jun/instagram-clone/src/App.js
@@ -1,15 +1,33 @@
 import React, { Component } from "react";
-//import  {}  from "./helpers";
+//import { print } from "./helpers";
+import Menu from "./components/Menu";
+import Data from "./data.json";
+import Motor from "./imgInSrc/img1.jpg";
 
 export default class App extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      userInput: "",
+      userName: "Hadi",
+    };
+    this.change = (e) => {
+      e.preventDefault();
+      this.setState({
+        userName: "Ali",
+      });
+    };
   }
 
   render() {
     return (
       <React.Fragment>
-        <h1>Hi I am React</h1>
+        <Menu propsName="Olga" info={Data} />
+        <form onSubmit={this.change}>
+          <h1>Hi I am {this.state.userName}</h1>
+          <input type="submit" />
+          <img src={Motor} alt="Hi" />
+        </form>
       </React.Fragment>
     );
   }
